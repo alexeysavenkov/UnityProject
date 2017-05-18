024f1f0cad16df87135014c985b6c4f4709634ef
Alexey Savenkov <alexey199753@gmail.com>
2017-05-19 00:45:58 +0300

    task 1: box colliders, rigid body, keyboard control, image flipping, moving camera

---
+++ Assets/Scripts/HeroFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+	
+	public GameObject rabbit;
+
+	void Update () {
+		//Отримуємо доступ до компонента Transform
+		//це Скорочення до GetComponent<Transform>
+		Transform rabbitTransform = rabbit.transform;
+
+		//Отримуємо доступ до компонента Transform камери
+		Transform cameraTransform = this.transform;
+
+		//Отримуємо доступ до координат кролика
+		Vector3 rabitPosition = rabbitTransform.position;
+		Vector3 cameraPosition = cameraTransform.position;
+
+		//Рухаємо камеру тільки по X,Y
+		cameraPosition.x = rabitPosition.x;
+		cameraPosition.y = rabitPosition.y;
+
+		//Встановлюємо координати камери
+		cameraTransform.position = cameraPosition;
+	}
+}
