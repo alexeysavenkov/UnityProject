dd33287b51e090b869ef0bfb66d3643f7956096b
Alexey Savenkov <alexey199753@gmail.com>
2017-06-03 11:14:01 +0300

    orcs opponents; a little buggy

---
+++ Assets/Scripts/Carrot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrot : Bomb {
+
+	public float speed = 5;
+
+	void Start() {
+		StartCoroutine (destroyLater ());
+	}
+
+	IEnumerator destroyLater() {
+		yield return new WaitForSeconds (3.0f);
+		Destroy (this.gameObject);
+	}
+
+	public void launch(Vector2 direction) {
+		Rigidbody2D myBody = this.GetComponent<Rigidbody2D> ();
+
+		Vector3 pos = this.transform.position;
+
+		myBody.velocity = direction.normalized * speed;
+
+		if (direction.x >= 0) {
+			this.transform.position += new Vector3 (1, 1, 0);
+		} else {
+			this.transform.position += new Vector3 (1, -1, 0);
+		}
+	}
+}
